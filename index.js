315ebe8dba0354e1d61ffb64ea5c5b4f2739c9a3
jacinta-18 <akpanjacinta6@gmail.com>
2023-07-11 11:21:21 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const HOUR = document.getElementById("hour")
+const MINUTES = document.getElementById("Minutes")
+const SECONDS = document.getElementById("Seconds")
+const AMPM = document.getElementById("ampm")
+
+
+function updateClock(){
+    let h = new   Date().getHours();
+    let m = new   Date().getMinutes();
+    let s = new   Date().getSeconds();
+     let ampm = "AM";
+
+
+     if (h > 12){
+        h = h - 12;
+        ampm = "PM";
+     }
+     h = h<10 ? "0" + h : h;
+     m = m<10 ? "0" + m : m;
+     s = s<10 ? "0" + s : s;
+
+
+     HOUR.innerText = h;
+     MINUTES.innerText = m;
+     SECONDS.innerText = s;
+     AMPM, (innerText = ampm);
+     setTimeout(() => {
+     updateClock()
+      
+     }, 1000);
+
+    
+
+}
+
+updateClock()
